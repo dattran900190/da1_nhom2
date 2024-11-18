1000cb6291e7826c773f749d429c406e31cb6dc5
kienxv <kienxvph49681@gmail.com>
2024-11-18 22:34:25 +0700

    boSuuTapLan1

--- admin/models/boSuuTap.php
+++ admin/models/boSuuTap.php
@@ -1,5 +1,83 @@
 <?php 
+class adminBoSuuTap {
+    public $conn;
+    public function __construct()
+    {
+        $this->conn = connectDB();
+    }
+    public function getAllBoSuuTap()
+    {
+        try {
+            $sql = "SELECT * FROM bo_suu_tap";
 
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute();
+            return $stmt->fetchAll();
+        } catch (PDOException $e) {
+            echo "CÓ LỖI: " . $e->getMessage();
+        }
+    }
+    public function postAddBoSuuTap($ten_bo_suu_tap, $mo_ta)
+    {
+        try {
+            $sql = "INSERT INTO bo_suu_tap (ten_bo_suu_tap, mo_ta) VALUES (:ten_bo_suu_tap, :mo_ta)";
+                
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute([
+                ':ten_bo_suu_tap' => $ten_bo_suu_tap,
+                ':mo_ta' => $mo_ta,
+                
+                
+            ]);
+            return true;
+        } catch (Exception $e) {
+            echo "CÓ LỖI:" . $e->getMessage();
+        }
+    }
+    public function formEditBoSuuTap($id){
+        try {
+            $sql = "SELECT * FROM bo_suu_tap WHERE id = :id";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute([
+                ':id'=>$id,
+            ]);
+            return $stmt->fetch();
+        } catch (Exception $e) {
+            echo "CÓ LỖI:".$e->getMessage();
+        } 
+    }
+    public function updateBoSuuTap($id, $ten_bo_suu_tap, $mo_ta) {
+        try {
+            $sql = "UPDATE bo_suu_tap
+            SET ten_bo_suu_tap = :ten_bo_suu_tap,
+            mo_ta = :mo_ta 
+            WHERE id = :id";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute([
+                'ten_bo_suu_tap' => $ten_bo_suu_tap,
+                'mo_ta' => $mo_ta,
+                ':id' => $id,
+            ]);
+            return true;
+        } catch (Exception $e) {
+            echo "CÓ LỖI:".$e->getMessage();
+        } 
+    }
+
+
+    public function deleteBoSuuTap($id) {
+        try {
+            $sql = "DELETE FROM bo_suu_tap WHERE id = :id";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute([
+                ':id' => $id,
+            ]);
+            return true;
+        } catch (Exception $e) {
+            echo "CÓ LỖI:".$e->getMessage();
+        } 
+    }
+}
 
 
 ?>
